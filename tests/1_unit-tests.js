e61b2ff0a8c94b6b7d5d9c485be729847ca530f0
Keirwilson90 <35281212+Keirwilson90@users.noreply.github.com>
2023-02-11 12:58:40 -0500

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,54 +1,66 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
+suite('Unit Tests', function() {
+  suite('Basic Assertions', function() {
     // #1
-    test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+    /** 1- Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
+    test('#isNull, #isNotNull', function() {
+      assert.isNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
     });
     // #2
-    test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'A string is not undefined');
+    /** 2- Use assert.isDefined() or assert.isUndefined () to make the tests pass. **/
+    test('#isDefined, #isUndefined', function() {
+      assert.isDefined(null, 'null is not undefined');
+      assert.isUndefined(undefined, 'undefined IS undefined');
+      assert.isDefined('hello', 'A string is not undefined');
     });
     // #3
-    test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+    /** 3- Use assert.isOk() or assert.isNotOk() to make the tests pass.**/
+    test('#isOk, #isNotOk', function() {
+      assert.isNotOk(null, 'null is falsey');
+      assert.isOk("I'm truthy", 'A string is truthy');
+      assert.isOk(true, 'true is truthy');
     });
     // #4
-    test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+    /** 4- Use assert.isTrue() or assert.isNotTrue() to make the tests pass **/
+    // .isTrue(true) and .NotFalse(everything else) will pass.
+    // .isFalse() and .is NotFalse() also exist.
+    test('#isTrue, #isNotTrue', function() {
+      assert.isTrue(true, 'true is true');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotTrue({ value: 'truthy' }, 'A truthy object is NOT TRUE (neither is false...)');
     });
   });
 
   // -----------------------------------------------------------------------------
 
-  suite('Equality', function () {
+  suite('Equality', function() {
     // #5
-    test('#equal, #notEqual', function () {
-      assert.fail(12, '12', 'Numbers are coerced into strings with ==');
-      assert.fail({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.fail(6 * '2', '12');
-      assert.fail(6 + '2', '12');
+    /** .equal(), .notEqual() **/
+    // .equal() compares objects using '=='
+    test('#equal, #notEqual', function() {
+      assert.equal(12, '12', 'Numbers are coerced into strings with ==');
+      assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.equal(6 * '2', '12');
+      assert.notEqual(6 + '2', '12');
     });
     // #6
-    test('#strictEqual, #notStrictEqual', function () {
-      assert.fail(6, '6');
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * '2', 12);
-      assert.fail([1, 'a', {}], [1, 'a', {}]);
+    /** .strictEqual(), .notStrictEqual() **/
+    //.strict() compares objects using '=='
+    test('#strictEqual, #notStrictEqual', function() {
+      assert.notStrictEqual(6, '6');
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * '2', 12);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
-    test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
+    //** .deepEqual(), .notDeepEqual() **/
+    // .deepEqual() asserts that two object are deep equal
+    test('#deepEqual, #notDeepEqual', function() {
+      assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
+      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
     });
   });
 
@@ -58,13 +70,14 @@ suite('Unit Tests', function () {
     return 1 + delta - Math.random();
   }
 
-  suite('Comparisons', function () {
+  suite('Comparisons', function() {
     // #8
-    test('#isAbove, #isAtMost', function () {
-      assert.fail('hello'.length, 5);
-      assert.fail(1, 0);
-      assert.fail(Math.PI, 3);
-      assert.fail(1 - Math.random(), 1);
+    /** 8- .isAbove()=> a > b, .isAtMost() => a <=b **/
+    test('#isAbove, #isAtMost', function() {
+      assert.isAtMost('hello'.length, 5);
+      assert.isAbove(1, 0);
+      assert.isAbove(Math.PI, 3);
+      assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
     test('#isBelow, #isAtLeast', function () {
